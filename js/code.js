4c652717567edf04e818b2350c605fe9333e3c8d
Vellixd <babicdmitrij21@gmail.com>
2025-04-22 06:56:55 +0300

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,24 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Модалка 1
+ 
     const modal1 = {
       openBtn: document.querySelector("[data-modal-open]"),
       closeBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
     };
-  
-    // Модалка 2
+
     const modal2 = {
       openBtn: document.querySelector("[data-modal2-open]"),
       closeBtn: document.querySelector("[data-modal2-close]"),
       modal: document.querySelector("[data-modal2]"),
     };
-  
-    // Функция-переключатель
+ 
     function toggleModal(modalRef) {
       modalRef.modal.classList.toggle("is-open");
     }
-  
-    // Навешиваем обработчики
+ 
     if (modal1.openBtn && modal1.closeBtn && modal1.modal) {
       modal1.openBtn.addEventListener("click", () => toggleModal(modal1));
       modal1.closeBtn.addEventListener("click", () => toggleModal(modal1));
